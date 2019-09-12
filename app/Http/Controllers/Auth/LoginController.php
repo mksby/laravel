472b7a09e18fd69ce76e49e9c6d7531cf8e38a77
MacPherson <maxim.troitsky@gmail.com>
2019-09-13 00:38:30 +0300

    add swagger

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,27 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+/**
+* @OA\Info(title="API", version="0.0.1")
+**/
+
+/**
+ * @OA\Post(
+ *     path="/login",
+ *     description="auth",
+ *     @OA\Parameter(
+ *          name="eventID",
+ *          in="query",
+ *          required=false,
+ *          description="The event ID specific to this event",
+ *          @OA\Schema(
+ *              type="string"
+ *          ),
+ *     ),
+ *     @OA\Response(response="default", description="Welcome page")
+ * )
+ */
+
 class LoginController extends Controller
 {
     /*
--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -49,7 +49,6 @@ return [
                 \Illuminate\View\Middleware\ShareErrorsFromSession::class,
                 \App\Http\Middleware\VerifyCsrfToken::class,
                 \Illuminate\Routing\Middleware\SubstituteBindings::class,
-                \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
                 'auth',
             ],
             'asset' => [],
